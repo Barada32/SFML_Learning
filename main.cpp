ad3c1442e8611033789dc9d0bfff9e9b80ea5ae2
Barada32 <barada.90@yandex.ru>
2023-03-20 18:40:14 +0300

    ВсеРаботает_14урок

--- main.cpp
+++ main.cpp
@@ -267,7 +267,7 @@ int main()
 					if ((TileMap[i][j] == '0')) s_map.setTextureRect(IntRect(64, 0, 32, 32));
 					if ((TileMap[i][j] == 'f')) s_map.setTextureRect(IntRect(96, 0, 32, 32));
 					if ((TileMap[i][j] == 'h')) s_map.setTextureRect(IntRect(128, 0, 32, 32));
-					s_map.setPosition(j * 32, i * 32);
+					s_map.setPosition(j * 32, i * 32); 
 
 					window.draw(s_map);
 				}
